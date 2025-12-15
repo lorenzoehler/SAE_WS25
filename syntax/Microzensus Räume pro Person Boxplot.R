e0300f0c44724e74c4f069bd419fb383b73b403f
Niklas-Biegert <niklas.biegert@gmail.com>
2025-12-15 14:17:35 +0100

    Create Microzensus Räume pro Person Boxplot.R

---
+++ syntax/Microzensus Räume pro Person Boxplot.R
@@ -0,0 +1,33 @@
+library(haven)
+EH2021_Vivienda <- read_sav("~/GitHub/SAE_WS25/data_raw/2021_Encuesta_de_hogares_2021_Microcensus/EH2021_Vivienda.sav")
+View(EH2021_Vivienda)
+head(EH2021_Vivienda)
+
+library(haven)
+library(dplyr)
+library(psych)
+
+names(EH2021_Vivienda)
+
+
+
+library(haven)
+library(dplyr)
+
+# Datensatz laden
+EH2021_Vivienda <- read_sav("~/GitHub/SAE_WS25/data_raw/2021_Encuesta_de_hogares_2021_Microcensus/EH2021_Vivienda.sav")
+
+# Wealth Index: Räume pro Person
+EH2021_Vivienda <- EH2021_Vivienda %>%
+  mutate(
+    wealth_index_rooms_per_person = s07a_24  / totper
+  )
+
+
+# Ergebnisse prüfen
+summary(EH2021_Vivienda$wealth_index_rooms_per_person)
+hist(EH2021_Vivienda$wealth_index_rooms_per_person,
+     main = "Wealth Index: Räume pro Person",
+     xlab = "Räume pro Person",breaks = 10
+     )
+
